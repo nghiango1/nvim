5d2176036191d382a95b960ba8e8be092bdd92f5
nghiango1 <ducnghia.tin47@gmail.com>
2024-03-18 22:47:27 +0700

    new place here

--- after/plugin/iig.lua
+++ after/plugin/iig.lua
@@ -1,7 +1,7 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.interingo = {
   install_info = {
-    url = "/home/osadmin/workspace/interpreter/tree-sitter-interingo", -- local path or git repo
+    url = "/home/osadmin/workspace/InterinGo/tree-sitter-interingo", -- local path or git repo
     files = { "src/parser.c" },                                        -- note that some parsers also require src/scanner.c or src/scanner.cc
     -- optional entries:
     branch = "main",                                                   -- default branch in case of git repo if different from master
