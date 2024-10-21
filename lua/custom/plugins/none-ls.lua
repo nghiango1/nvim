c72368dc3b4e7645b8e950d1f0e3d2fa22b32b2f
ylong <ducnghia.tin47@gmail.com>
2024-10-21 21:27:57 +0700

    add none-ls

---
+++ lua/custom/plugins/none-ls.lua
@@ -0,0 +1,15 @@
+return {
+  "nvimtools/none-ls.nvim",
+  -- Optional dependency
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.erb_format,
+        null_ls.builtins.formatting.erb_lint,
+      },
+    }
+  end,
+}
